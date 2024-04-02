8ef7e06af33990f3aff968123b39b36de8d08c73
Dawid <musiald20@gmail.com>
2024-04-02 18:16:07 +0200

    Add project files.

---
+++ c#ApiLayout/Controllers/Dtos.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+public class UserDto
+{
+    [BindNever]
+    public string name { get; set; }
+    public string email { get; set; }
+    public string description { get; set; }
+}
---
+++ c#ApiLayout/Utilities/logEvent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace c_ApiLayout.Utilities
+{
+    public class Log
+    {
+        public static void LogEvent(IMongoCollection<BsonDocument> logCollection, string name, string email, string description, int ticketNum)
+        {
+            var log = new BsonDocument
+         {
+             { "name", name },
+             { "email", email },
+             { "description", description },
+             {"ticket", ticketNum }
+         };
+            logCollection.InsertOne(log);
+        }
+    }
+}
